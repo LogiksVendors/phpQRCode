7877e637d6f76edd2fcb6efee939534d5310209b
Bismay Kumar Mohapatra <bismay4u@gmail.com>
2024-04-04 01:14:49 +0530

    Modified the Loading of phpqrcode library

--- boot.php
+++ boot.php
@@ -3,6 +3,6 @@ if(!defined('ROOT')) exit('No direct script access allowed');
 
 //http://phpqrcode.sourceforge.net/
 
-include_once __DIR__."/phpqrcode/qrlib.php";
-
+//include_once __DIR__."/phpqrcode/qrlib.php";
+include_once __DIR__."/phpqrcode/phpqrcode.php";
 ?>
